aa459329f4a3a7969abebaf4d2558abf835b8cf3
Jack <trickytrees97@icloud.com>
2024-12-14 14:47:03 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# Pseudocode for the algorithm:
+# 1. Request input from the user to store as 'password'
+# 2. Request a second input to compare with the password
+# 3. If second input matches password, output password and incorrect inputs
+# 4. If second input does not match, store the incorrect input and prompt again
+# 5. Repeat until correct password is entered and output the results
+
+Username = input ("Please enter your Username")
+print("your username is:", Username)
+
+Age = input("Please insert your age?")
+print = ("Your Age is:", Age)
+
+print ("Hello World!") 
